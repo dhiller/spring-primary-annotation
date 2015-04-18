6c86fbbe68377c704f680440ba110ee24cc77742
Daniel Hiller <daniel.hiller.1972@gmail.com>
2015-04-18 09:15:11 +0200

    Minor changes
    - Added target dir to git ignore
    - Removed unused imports
    - Added exec:java to verify phase

--- overridden-bean/src/main/java/de/dhiller/spring/bugs/Application.java
+++ overridden-bean/src/main/java/de/dhiller/spring/bugs/Application.java
@@ -1,11 +1,9 @@
 package de.dhiller.spring.bugs;
 
-import de.dhiller.spring.bugs.overriding_bean.OverridingBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 public class Application {
 
